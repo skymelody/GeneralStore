2f5802406f4e26bb176790e38631a2d2cb5f49bf
skymelody <lzfgnu@gmail.com>
2021-02-17 16:51:44 +0800

    update vim config

---
+++ configs/plugin_install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/sh
+
+declare -a plugin_urls=(
+"https://github.com/preservim/nerdtree"
+"https://github.com/vim-airline/vim-airline"
+"https://github.com/vim-airline/vim-airline-themes"
+)
+
+echo "plugins install begin"
+
+plugin_install_path_root="$HOME/.vim/pack/plugins/start"
+declare -a plugin_install_paths=()
+
+for plugin in "${plugin_urls[@]}"
+do 
+  plugin_install_path="${plugin_install_path_root}/${plugin##*/}"
+  plugin_install_paths+=("${plugin_install_path}")
+
+  if [ ! -d "$plugin_install_path" ]; then
+    mkdir -p "$plugin_install_path"
+  fi
+done
+
+
+index=0
+for plugin in "${plugin_urls[@]}"
+do
+  if [ ! -d "$plugin_install_path" ]; then
+    git clone "${plugin_urls[${index}]}" "${plugin_install_paths[${index}]}"
+  fi
+  index=$((index+1))
+done
+
+echo "plugins install end"
